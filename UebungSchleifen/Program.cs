e69920d1b10052219b5e07d7170b0c0a19b7c1f3
nake <oliver.nake@web.de>
2023-09-20 14:12:45 +0200

    Projektdateien hinzufügen.

---
+++ UebungSchleifen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UebungSchleifen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Schreibe eine for Schleife, die die Zahlen von 1 bis 10 ausgibt
+            //2. Schreibe eine while-Schleife, die die Großbuchstaben von A bis K ausgibt.
+            //3. Passe die beiden Schleifen so an, dass du das Schleifenende über die Konsole
+            //   bestimmen kannst.
+        }
+    }
+}
